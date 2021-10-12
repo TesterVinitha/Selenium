d4bba7bf1bdd9f41f98ae1ef114fb7f6ce5b8194
TesterVinitha <vinithalaxmi2021@gmail.com>
2021-10-12 13:58:47 +0530

    Adding Method

--- src/test/java/Sample/Calculator.java
+++ src/test/java/Sample/Calculator.java
@@ -2,4 +2,15 @@ package Sample;
 
 public class Calculator {
 
+	public void add() {
+
+		System.out.println("adding");
+	}
+
+	public static void main(String[] args) {
+
+		Calculator c = new Calculator();
+		c.add();
+	}
+
 }
