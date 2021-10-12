90a58cd2e5a63dfd78ad1e12b179ae1273e44be1
TesterVinitha <vinithalaxmi2021@gmail.com>
2021-10-12 14:03:48 +0530

    Sub Method

--- src/test/java/Sample/Calculator.java
+++ src/test/java/Sample/Calculator.java
@@ -2,4 +2,14 @@ package Sample;
 
 public class Calculator {
 
+	public void sub() {
+		System.out.println("sub");
+	}
+
+	public static void main(String[] args) {
+
+		Calculator c = new Calculator();
+		c.sub();
+	}
+
 }
